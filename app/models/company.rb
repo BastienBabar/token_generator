4716d4190f52064aca933a19d54a5d1f2fa1820d
Bastien Barbé <bb@12auto.de>
2018-01-18 10:03:42 +0100

    gemify token started

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,8 @@
 class Company < ApplicationRecord
+  tokenize! attribute: 'identity', separator: ':', block_size: 4, number_of_blocks: 2
+
   has_many :employees, dependent: :destroy
 
   validates_presence_of :name
 
-  before_create :generate_identity
-
-  def generate_identity
-    self.identity ||= Identifier::Token.new(self.class, 'identity',{ separator: ':', block_size: 4, number_of_blocks: 2 }).generate
-  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,11 +1,11 @@
+
+# require "identifier/active_record/token"
+
 class Employee < ApplicationRecord
+  tokenize! attribute: 'identifier', separator: '-', block_size: 4, number_of_blocks: 3
+
   belongs_to :company, touch: true
 
   validates_presence_of :first_name, :last_name
 
-  before_create :generate_identifier
-
-  def generate_identifier
-    self.identifier ||= Identifier::Token.new(self.class, 'identifier',{ separator: '-', block_size: 4, number_of_blocks: 3 }).generate
-  end
 end
